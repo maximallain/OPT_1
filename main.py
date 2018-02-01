1bd24aff206c7d1f95f65d608c48d878d4a8372f
maximallain <maxime.allain@student.ecp.fr>
2018-02-01 14:41:39 +0100

    File main cleaned

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from sequence_finder import sequence_finder
 from constraint_programming import constraint_programming
 from dict_vocabulary import dict_vocabulary
 from domain_var import domain_var
-from segments_relations import segments_relations
+from print_res import print_res
+from constraints import constraints
 import string
 
 """
 1. On récupère les segments h et v
 2. On récupère les mots de vocabulaire du fichier
-3. On définit le domaine
+3. On définit le domainew
 4. Création du programme
 """
 
@@ -30,19 +31,8 @@ for key in dict_intersection.keys() :
     var[key] = set(alphabet)
 
 P = constraint_programming(var)
+constraints(dict_intersection, P)
 
-for key, value in dict_intersection.items():
-    case = key
-    seg_h = list(value.keys())[0]
-    seg_v = list(value.keys())[1]
-    pos_h = list(value.values())[0]
-    pos_v = list(value.values())[1]
-    domain_constr_h = {(word_h, word_h[pos_h]) for word_h in P.var[seg_h]}
-    domain_constr_v = {(word_v, word_v[pos_v]) for word_v in P.var[seg_v]}
-    constr_h = (seg_h, case, domain_constr_h)
-    constr_v = (seg_v, case, domain_constr_v)
-    P.addConstraint(seg_h, case, domain_constr_h)
-    P.addConstraint(seg_v, case, domain_constr_v)
 time1 = time.time()
 result = P.solve()
 time2 = time.time()
@@ -57,36 +47,7 @@ print("Temps de résolution : {}".format(time2-time1))
 result = P.solve()
 print(result)
 
-case_final = {}
-for segments_h, list_pos_letter_h in segments_h.items() :
-    i = 0
-    word_h = result[segments_h]
-    for elt in list_pos_letter_h :
-        case_final[elt] = word_h[i]
-        i += 1
-print(case_final)
-for segments_v, list_pos_letter_v in segments_v.items() :
-    i = 0
-    word_v = result[segments_v]
-    for elt in list_pos_letter_v :
-        case_final[elt] = word_v[i]
-        i += 1
 
+print_res = print_res(segments_h, segments_v, result, filecross)
 
-with open(filecross, 'r') as fc :
-    len_cross = len(fc.readlines())
-
-str_final = ""
-for i in range(0,len_cross) :
-    str_temp = ""
-
-    for j in range(0, len_cross) :
-        var_temp = "x"+str(i)+"."+str(j)
-        if var_temp in case_final :
-            str_temp += case_final[var_temp]+" "
-        else :
-            str_temp += "# "
-
-    str_final += str_temp + "\n"
-
-print(str_final)
+print(print_res)
